b05b41b2aaff62112049fd4766b36247eabb8e51
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2021-03-23 01:40:08 -0300

    App finalizada de acordo com as aulas do bloco 2

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,10 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useContext, useState } from 'react';
 import Modal from 'react-modal';
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { api } from '../../services/api';
+import { useTransactions } from '../../hooks/UseTransactions';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -12,22 +12,29 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const { createTransaction } = useTransactions();
+
   const [title, setTitle] = useState('');
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = {
+    await createTransaction({
       title,
-      value,
+      amount,
       category,
       type,
-    }
+    });
 
-    api.post('transactions', data);
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit');
+
+    onRequestClose();
   }
 
   return (
@@ -51,8 +58,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
           <input 
             type="number" placeholder="Valor"
-            value={value}
-            onChange={event => setValue(Number(event.target.value))}
+            value={amount}
+            onChange={event => setAmount(Number(event.target.value))}
           />
 
           <TransactionTypeContainer>
@@ -73,7 +80,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
               onClick={() => { setType('withdraw'); }}
             >
               <img src={outcomeImg} alt="Entrada"/>
-              <span>Entrada</span>
+              <span>Saída</span>
             </RadioBox>
           </TransactionTypeContainer>
 
